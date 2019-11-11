ce3371e2128b54e16b1768ebd2455eb60fd190f3
EricVincitore <epv1001@verizon.net>
2019-11-10 19:48:56 -0500

    added pseudo code for javascript

--- assets/javascript/gif-app.js
+++ assets/javascript/gif-app.js
@@ -17,5 +17,16 @@ $.ajax({
 
 // 1. array of animals to loop through for buttons
 // 2. var to pick up what animal the user adds through the search form
-// 3. var for api url including api key
-// 4. function sto start working with the api 
+//      a. adds user animal input to array
+//      a. does not add if animal is already in the array
+//      b. alert user if animal is already in the array
+// 3. make buttons dynamically of each animal in the animal array
+// 4. var for api url including api key
+// 5. function to start working with the api 
+// 6. on click event to post 10 gifs to card
+//      a. clear div that gifs go in
+//      b. take input and search giphy
+//      c. filter results by rating
+//      d. filter to only show 10 results
+//      e. on click to start and stop gif
+//      f. add css to make all cards the same size
