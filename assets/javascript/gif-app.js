1697256edf018ff4138b94d7e5724fdc7c06c8e6
EricVincitore <epv1001@verizon.net>
2019-11-13 20:02:03 -0500

    site now calls to api correctly and displays gifs

--- assets/javascript/gif-app.js
+++ assets/javascript/gif-app.js
@@ -1,11 +1,11 @@
 //ensures dom is ready
 $(document).ready(function () {
-  
+
   //starting buttons
   btnLoop();
 
   //event listener for enter key
-  $(document).on("keypress",function(e) {
+  $(document).on("keypress", function (e) {
     if (e.which == 13) {
 
       //stops page reload/takes input
@@ -34,9 +34,6 @@ function btnLoop() {
     //adds class & bootstrap
     gifBtn.addClass("btn btn-success gifTrigger");
 
-    //button data attribute
-    gifBtn.attr("data-letter", animals[i]);
-
     //button text
     gifBtn.text(animals[i]);
 
@@ -49,7 +46,7 @@ function btnLoop() {
 
 //function to take user input of animal
 function getAnimal() {
-  
+
   //takes input value and clears form
   var search = $(".form-control").val().trim();
   $(".form-control").val("");
@@ -74,21 +71,43 @@ function getAnimal() {
   }
 };
 
-$(".reset-btn").click(function () {
+$(".btn-danger").click(function () {
+
+  $(".gif-div").empty();
+
+});
+
+var cors = "https://cors-anywhere.herokuapp.com/";
+
+function getGif() {
+  var animalSearch = $(this).text().trim();
+  var queryURL = cors + "api.giphy.com/v1/gifs/search?&q=" + animalSearch + "&api_key=7Wkp2d9UIXdEznvgxR7roo4cBBH7gQC6";
   
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+    var data = response.data;
+    for (let i = 0; i < data.length; i++) {
+      let topicDiv = $("<div class='topicDiv d-flex'>");
+      let rating = $("<p>").text("Rating: " + data[i].rating);
+      topicDiv.append(rating);
+      let img1 = $("<img>");
+      img1.attr("src", data[i].images.fixed_height_still.url);
+      let img2 = $("<img>");
+      img2.attr("src", data[i].images.fixed_height.url);
+      img2.hide();
+      topicDiv.append(img1, img2);
+      $(".gif-div").prepend(topicDiv)
+    }
+  });
+};
+$(document).on("click", ".success-btn", getGif)
+$(document).on("click", "img", function () {
+  $(this).hide();
+  $(this).siblings().show();
 })
-
-//var animalSearch = $(this).attr("data-letter");
-
-//var queryURL = "api.giphy.com/v1/gifs/search?&q=" + animalSearch +"&api_key=7Wkp2d9UIXdEznvgxR7roo4cBBH7gQC6";
-
-// $.ajax({
-//   url: queryURL,
-//   method: "GET"
-// }).then(function (response) {
-//   console.log(response);
-// });
-
 
 // *** 1. array of animals to loop through for buttons
 // *** 2. var to pick up what animal the user adds through the search form
@@ -97,7 +116,7 @@ $(".reset-btn").click(function () {
 // ***      b. alert user if animal is already in the array
 // *** 3. make buttons dynamically of each animal in the animal array
 // 4. var for api url including api key
-// 5. function to start working with the api 
+// *** 5. function to start working with the api 
 // 6. on click event to post 10 gifs to card
 //      a. clear div that gifs go in
 //      b. take input and search giphy
